e9b5d3da6932b4e80efe6a5d21d8ebf19b0a0450
shaunmcarmody <shaun.carmody@live.co.uk>
2019-03-29 01:29:54 +0000

    initialise crud operations

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const cors = require('cors');
 const helmet = require('helmet');
 const server = express();
 
+const dishes = require('./dishes/dishesRoute.js');
+const recipes = require('./recipes/recipesRoute.js');
+
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.use('/api/dishes', dishes);
+server.use('/api/recipes', recipes);
 
 server.listen(5000, console.log('Listening on port 5000'));
